eb264718f8350acc3a540b5d56f8c25be3714417
Sam Krick <160279286+samkrick25@users.noreply.github.com>
2025-09-22 13:02:36 -0400

    name_json added

---
+++ reconstructions/name_json.py
@@ -0,0 +1,18 @@
+import json
+import os
+folder1 = r"C:\Users\samkr\OneDrive\Documents\BU\AllenExASPIM\medulla_IRN_PRN_PGRN\json\json"
+folder2 = r"C:\Users\samkr\OneDrive\Documents\BU\AllenExASPIM\medulla_IRN_PRN_PGRN\json_w_names"
+for filename in os.listdir(folder1):
+    file  = os.path.join(folder1, filename)
+    with open(file, 'r') as f:
+        data = json.load(f)
+        #print(type(data['neurons']))
+        neuron = data['neurons']
+        meta = neuron[0]
+        #print(type(meta))
+        meta['idString'] = filename.replace('.json','')
+        #print(meta['idString'])
+        new_name = meta['idString'] + '.json'
+        full_name = os.path.join(folder2, filename)
+        with open(full_name, 'w') as f2:
+            json.dump(data, f2)
